b42075e42ecece87d3170635d1473f91befcb40d
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-04-23 20:00:27 +0300

    Menünün UI'da Listelenmesi

---
+++ Yummy.UI/DTOs/ProductDTOs/ResultProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Yummy.UI.DTOs.ProductDTOs
+{
+    public class ResultProductDTO
+    {
+        public int ProductID { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public int CategoryID { get; set; }
+    }
+}
---
+++ Yummy.UI/ViewComponents/DefaultViewComponents/_MenuProductDefaultComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Yummy.UI.DTOs.ProductDTOs;
+using Yummy.UI.Helpers;
+
+namespace Yummy.UI.ViewComponents.DefaultViewComponents
+{
+    public class _MenuProductDefaultComponentPartial : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultProductDTO>>("Products/visible-on-home");
+            return View(values);
+        }
+    }
+}
