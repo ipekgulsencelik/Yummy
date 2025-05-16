40d3f734d474d848239bbab117adf25ce7b0d956
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-05-16 15:23:04 +0300

    Footer

---
+++ Yummy.UI/ViewComponents/DefaultViewComponents/_ScriptDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.UI.ViewComponents.DefaultViewComponents
+{
+    public class _ScriptDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
